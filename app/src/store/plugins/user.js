97668b789986e9d4baf7316f4bcd8b75bb308131
zxlfly <zxlfly@foxmail.com>
2021-01-04 11:51:12 +0800

    update

--- app/src/store/plugins/user.js
+++ app/src/store/plugins/user.js
@@ -10,6 +10,7 @@ export default store=>{
     // 如果状态发生改变，自动存入localstoreage
     store.subscribe((mutation,state)=>{
         if(mutation.type==='login'){
+            // 这个地方state代表所有的，取值的时候注意
             const user = JSON.stringify(state.user)
             localStorage.setItem('user',user)
         }else if(mutation.type==='logout'){
